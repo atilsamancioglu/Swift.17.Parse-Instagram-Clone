e9c9978edbdd5c6d62c62a3ac7b2f472a0de70a0
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2017-02-06 19:57:40 +0300

    Initial Commit

---
+++ Parse Instagram Clone/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Parse Instagram Clone
+//
+//  Created by Atıl Samancıoğlu on 06/02/2017.
+//  Copyright © 2017 Atıl Samancıoğlu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Parse Instagram Clone/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Parse Instagram Clone
+//
+//  Created by Atıl Samancıoğlu on 06/02/2017.
+//  Copyright © 2017 Atıl Samancıoğlu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
